6bd28d0b0b1e7bf21268b7345549d820d8540914
Egor Khamitsevich <lunexe@gmail.com>
2022-08-16 23:05:11 +0300

    feat:compleate

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.numMiddle = 0;
+    }
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
+        return Math.round((this.min + this.max) / 2);
 
     }
 
     lower() {
+        this.max = Math.round((this.min + this.max) / 2);
 
     }
 
     greater() {
+        this.min = Math.round((this.min + this.max) / 2);
 
     }
 }
